b450ff0c69c1b42c1ffc82c9fc6c853e6b64490d
Iasmim Marinho <72471500+IasmimMarinho@users.noreply.github.com>
2022-05-11 22:22:50 -0300

    Documentação inicial para a classe de Pergunta

--- TRIVIA_GAME/src/TRIVIA_GAME/Pergunta.java
+++ TRIVIA_GAME/src/TRIVIA_GAME/Pergunta.java
@@ -1,7 +1,7 @@
 package TRIVIA_GAME;
 public class Pergunta {
     String pergunta = null;
-    
+
     String respostaUm = null;
     String respostaDois = null;
     String respostaTres = null;
@@ -9,6 +9,17 @@ public class Pergunta {
     
     String respostaCerta = null;
     
+     /**
+     * Construtor de Pergunta. Para criar uma pergunta é necessária uma descrição para a mesma
+     * e 5 opções de respostas, além de indicar a opção correta.
+     * @param pergunta recebe a descrição da pergunta.
+     * @param respostaUm opção 1 de resposta.
+     * @param respostaDois opção 2 de resposta.
+     * @param respostaTres opção 3 de resposta.
+     * @param respostaQuatro opção 4 de resposta.
+     * @param respostaCerta opção de resposta correta.
+     * 
+     */
     public Pergunta(String pergunta, String respostaUm, String respostaDois,
             String respostaTres, String respostaQuatro, String respostaCerta) {
             
