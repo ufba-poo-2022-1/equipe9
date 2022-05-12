7459c929a69e71302e11c5a310421c78a85a0386
Iasmim Marinho <72471500+IasmimMarinho@users.noreply.github.com>
2022-05-11 22:06:57 -0300

    Documentação básica inicial para classe Usuario

--- TRIVIA_GAME/src/TRIVIA_GAME/Usuario.java
+++ TRIVIA_GAME/src/TRIVIA_GAME/Usuario.java
@@ -4,8 +4,20 @@ public class Usuario {
     private String nome, login, tipo;
     String senha;
 
+    /**
+     * Construtor sem parametros.
+     */
     public Usuario(){
     }
+
+    /**
+     * Construtor usuário.
+     *
+     * @param nome nome do usuário 
+     * @param login login de acesso do usuário 
+     * @param senha senha de acesso do usuário 
+     * @param tipo identifica o tipo de usuário 
+     */
 
     public Usuario (String nome, String login, String senha, String tipo){
         this.nome = nome;
@@ -13,19 +25,37 @@ public class Usuario {
         this.senha = senha;
         this.tipo = tipo;
     }
-
+    
+    /**
+     * Retorna nome do usuário.
+     * @param nome nome do usuário
+     * @return retorna o nome
+     */
     public String getNome() {
         return nome;
     }
-
+     /**
+     * Retorna login do usuário.
+     * @param login login do usuário
+     * @return retorna o login
+     */
     public String getLogin() {
         return login;
     }
+     /**
+     * Retorna senha do usuário.
+     * @param senha senha do usuário
+     * @return retorna a senha
+     */
 
     public String getSenha() {
         return senha;
     }
-
+     /**
+     * Retorna tipo do usuário.
+     * @param tipo tipo do usuário
+     * @return retorna o tipo
+     */
     public String getTipo() {
         return tipo;
     }
