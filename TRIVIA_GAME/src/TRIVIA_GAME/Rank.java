adffa2816201dc81a4d484dec0b9974b46ddc611
Iasmim Marinho <72471500+IasmimMarinho@users.noreply.github.com>
2022-06-13 22:17:42 -0300

    documentando Rank class

--- TRIVIA_GAME/src/TRIVIA_GAME/Rank.java
+++ TRIVIA_GAME/src/TRIVIA_GAME/Rank.java
@@ -3,6 +3,12 @@ package trivia_bot;
 import java.util.Comparator;
 import java.util.ArrayList;
 
+/**
+ * compara jogadores por pontuação. 
+ * @param o1 primeiro jogador
+ * @param o2 segundo jogador
+ * @return  ordem que o jogador ocupa no rank 
+ */
 public class Rank implements Comparator<Jogador>{
 
     public int compare(Jogador o1, Jogador o2)
