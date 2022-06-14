6cd332fdc9ca6a0d09151ab77c7c2c9f8f7c0b7d
Iasmim Marinho <72471500+IasmimMarinho@users.noreply.github.com>
2022-06-13 21:40:39 -0300

    Documentando Jogador class

--- TRIVIA_GAME/src/TRIVIA_GAME/Jogador.java
+++ TRIVIA_GAME/src/TRIVIA_GAME/Jogador.java
@@ -4,28 +4,38 @@ public class Jogador extends Usuario{
 
 	 private int pontuacao=0;
 
-
+		/**
+		* Construtor Jogador.
+		* @param nome nome do jogador
+		*/
 	    public Jogador(String nome) {
 	    	super(nome);
 	    }
 
-
+		/**
+	     * Retorna pontuacao do usuário.
+	     * @return retorna a pontuacao
+	     */
 	    public int getPontuacao() {
 	        return this.pontuacao;
 	    }
-	    /**
-	     * Retorna pontuacao do usuário.
-	     * @param pontuacao pontuacao do usuário
-	     * @return retorna a pontuacao
-	     */
-
+	
 	    public void setPontuacao(int pontuacao) {
 	        this.pontuacao = pontuacao;
 	    }
+		/**
+	     * Incrementa pontuacao do usuário.
+		 * @param pontuacao pontuação por acerto
+	     */
 
 	    public void adicionarPontuacao(int pontuacao) {
 	        this.pontuacao += pontuacao;
 	    }
+		/**
+		* compara nome do jogador, o nome do jogador deve ser único.
+		* @param  jogador 
+		* @return retorna true or false para a comparação 
+		*/
 	    public boolean equalsNamejogador(Object jogador) {
 	        Jogador outroJogador = (Jogador)jogador;
 	        return this.getNome().equals(outroJogador.getNome());
