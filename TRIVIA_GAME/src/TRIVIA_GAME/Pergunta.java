4ad763da1263989af30955e393ad7b5f11e111ac
Lucas Morais Barreto <lucas.m.barreto12@gmail.com>
2022-06-19 12:58:43 -0300

    Restaurando alterações na classe de pergunta que foram sobrescritas com a transição pro Discordbot

--- TRIVIA_GAME/src/TRIVIA_GAME/Pergunta.java
+++ TRIVIA_GAME/src/TRIVIA_GAME/Pergunta.java
@@ -1,34 +1,74 @@
 package TRIVIA_GAME;
+import java.util.ArrayList;
 
-public class Pergunta{
-    String pergunta = null;
-    
-    String respostaUm = null;
-    String respostaDois = null;
-    String respostaTres = null;
-    String respostaQuatro = null;
-    
-    String respostaCerta = null;
-    /**
-     * Cria uma nova pergunta.
-     * @param pergunta descrição da pergunta
-     * @param respostaUm descrição opção 1
-     * @param respostaDois descrição opção 2
-     * @param respostaTres descrição opção 3
-     * @param respostaQuatro descrição opção 4
-     * @param respostaCerta descrição resposta certa
-     */
-    public Pergunta(String pergunta, String respostaUm, String respostaDois,
-            String respostaTres, String respostaQuatro, String respostaCerta) {
-            
+public class Pergunta {
+    private String pergunta;
+    private ArrayList<String> alternativas = new ArrayList<String>();
+    private String resposta; //indice maior que 1
+
+    // Construtor
+
+    public Pergunta(String pergunta,  ArrayList<String> alternativas , String resposta){
         this.pergunta = pergunta;
-        
-        this.respostaUm = respostaUm;
-        this.respostaDois = respostaDois;
-        this.respostaTres = respostaTres;
-        this.respostaQuatro = respostaQuatro;
-        
-        this.respostaCerta = respostaCerta;
+        this.alternativas = alternativas;
+        this.resposta = resposta;
     }
+
+    // Métodos
+
+    public void PrintPergunta(){
+		System.out.println(getPergunta());
+
+        //char c = 'A'; //primeira alternativa
+
+        for(String alt : getAlternativas()){
+            //System.out.print(c++ + ". "); //Prefixo da alternativa
+            System.out.println(alt);
+        }
+
+    }
+
+
+    // Getters
+
+    public String getPergunta(){
+        return pergunta;
+    }
+
+    public String getResposta(){
+        return resposta;
+    }
+
+    public ArrayList<String> getAlternativas(){ // Para todas as alternativas
+        return alternativas;
+    }
+
+    public String getAlternativa(String alt){ // Para uma alternativa específica
+
+        // Converte Char to Int. A = 1, B = 2....
+        int indice = (int) alt.toUpperCase().charAt(0) - 64;
+        return alternativas.get(indice); //indices de 1 a N
+    }
+
     
+    // Setters
+    
+    public void setPergunta(String pergunta){
+        this.pergunta = pergunta;
+    }
+
+    public void setResposta(String resposta){
+        this.resposta = resposta;
+
+    }
+
+    public void setAlternativa(int indice, String alternativa){ // Para uma alternativa específica
+        alternativas.set(indice+1, alternativa);
+    }
+
+    public void setAlternativas(ArrayList<String> alternativas){ // Para todas as alternativas
+        this.alternativas = alternativas;
+    }
+
+
 }
